8cd922cc4f3429b0697eb80ba9beb774c0696366
GS10071999 <g.shamatava1999@gmail.com>
2026-04-28 16:35:07 +0400

    Updated aiming point

--- Assets/Scripts/Weapons/Strela2M/Strela2MAimingPoint.cs
+++ Assets/Scripts/Weapons/Strela2M/Strela2MAimingPoint.cs
@@ -12,17 +12,17 @@ public class Strela2MAimingPoint : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //_strela.parent = _headView;
-        //_strela.localPosition = _objectTransformSettingsVRAiming.Position;
-        //_strela.localRotation = Quaternion.Euler(_objectTransformSettingsVRAiming.Rotation);
+        _strela.parent = _headView;
+        _strela.localPosition = _objectTransformSettingsVRAiming.Position;
+        _strela.localRotation = Quaternion.Euler(_objectTransformSettingsVRAiming.Rotation);
         Debug.Log("Should Switch Point");
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //_strela.parent = _weaponHolder;
-        //_strela.localPosition = _objectTransformSettingsVRHolding.Position;
-        //_strela.localRotation = Quaternion.Euler(_objectTransformSettingsVRHolding.Rotation);
-        // Debug.Log("Should Not Switch Point");
+        _strela.parent = _weaponHolder;
+        _strela.localPosition = _objectTransformSettingsVRHolding.Position;
+        _strela.localRotation = Quaternion.Euler(_objectTransformSettingsVRHolding.Rotation);
+        Debug.Log("Should Not Switch Point");
     }
 }
